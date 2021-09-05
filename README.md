b75f6e65ea0ed342fb8fdd84cc0fb4f3b1a17f38
Thiago <thiago.bussola@tindin.com.br>
2021-09-04 21:11:09 -0300

    finalizando setup inicial do servidor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Typescript API Template
 A generic api with nodejs, typescript and mongoDB to guide future projects.
+
+
+
+# Credits
+This api follows the concepts presented by Marcos Henrique da Silva in his article: https://www.toptal.com/express-js/nodejs-typescript-rest-api-pt-1
--- src/users/users.routes.config.ts
+++ src/users/users.routes.config.ts
@@ -1,7 +1,7 @@
 import { CommonRoutesConfig } from '../common/common.routes.config'
 import express from 'express'
 
-export class UserRoutes extends CommonRoutesConfig {
+export class UsersRoutes extends CommonRoutesConfig {
   constructor (app: express.Application) {
     super(app, 'UserRoutes')
   }
